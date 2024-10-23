278b0e9b69fc19cf92e17220990b3a10d43e21da
kaio santos <kaiojugno@gmail.com>
2024-10-22 23:13:25 -0300

    added body child components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BodyComponent } from './page/body/body.component';
+import { FooterComponent } from './page/footer/footer.component';
+import { HeaderComponent } from './page/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BodyComponent, FooterComponent, HeaderComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
